3f4502b9712532059ca018ad80cea0c5da6f8369
Dmitriy <gogolevdms@gmail.com>
2018-06-18 19:32:53 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,10 +1,17 @@
-var crowdsale = artifacts.require("crowdsale");
+const TokenContract = artifacts.require("./ENRTNToken.sol");
+const CrowdsaleContract = artifacts.require("./ENRTNCrowdsale.sol");
 
-contract('Crowdsale', async(accounts) {
-	beforeEach(async function() {
-		token = await ENRTN.deployed();
-	});
-	it('d', async function(){
+contract('ManagementContract', async (accounts) => {
 
-	});
+    beforeEach(async function() {
+        token = await TokenContract.deployed();
+        crowdsale = await CrowdsaleContract.deployed();
+    });
+
+    it('should return the correct totalSupply erc20 after construction', async function() {
+        let totalSupply_ = await token.totalSupply();
+        const pendingSupply = 72800000 * 10 ** 18;
+        assert.equal(totalSupply_.valueOf(),  pendingSupply, "total supply is not correct");
+    });
+
 });
